822c32b27edfdb82d28461f5ac46084ad354ad4f
Artem1KN <91216403+Artem1KN@users.noreply.github.com>
2022-06-04 17:03:14 +0500

    Update 216.cpp

--- 216.cpp
+++ 216.cpp
@@ -1,1 +1,32 @@
-10
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <fstream>
+#include <numeric>
+
+using namespace std;
+
+#define ut unsigned int
+
+int main()
+{
+    ifstream fin("input.txt");
+    ofstream fout("output.txt");
+
+    int n;
+    int sum = 0;
+    fin >> n;
+    vector<int> massiv(n);
+    for (int i = 0; i < n; i++) {
+        fin >> massiv[i];
+        sum += massiv[i];
+    }
+    sort(massiv.begin(), massiv.end());
+    int massivMax = massiv[n - 1];
+    int massivMin = min(sum / 2, sum - massivMax);
+    fout << massivMin;
+
+    return 0;
+}
